299042767d4b19530ff3d6d231312a2783604f52
Thomas Hänig <thomas.haenig@akquinet.de>
2018-01-18 11:07:11 +0100

    add different build tasks for umd and amd and create several folders in dist
    closes q2g/daVinci.js#60
    
    Signed-off-by: Thomas Hänig <thomas.haenig@akquinet.de>

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,34 @@ var package = require("./package.json");
 
 var gitVersion  = require("./gulpGitPlugin");
 
-gulp.task('less', function () {
+gulp.task('less-umd', function () {
   return gulp.src('./src/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     // .pipe(rename({dirname: ''}))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/umd'));
 });
 
-gulp.task('html', function () {
+gulp.task('less-amd', function () {
+    return gulp.src('./src/**/*.less')
+    .pipe(less({
+    paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    // .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('./dist/amd'));
+});
+
+gulp.task('html-umd', function () {
   return gulp.src('./src/**/*.html')
     // .pipe(rename({dirname: ''}))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/umd'));
+})
+
+gulp.task('html-amd', function () {
+    return gulp.src('./src/**/*.html')
+    // .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('./dist/amd'));
 })
 
 gulp.task("gitversionPackage", () => {  
@@ -39,12 +54,24 @@ gulp.task("gitversionPackage", () => {
     });
 });
   
-gulp.task("addVersionNumber", () => {
+gulp.task("addVersionNumber-umd", () => {
     gitVersion.getFullVersionString()
     .then((res) => {
-            return gulp.src('./dist/daVinci.js')
+            return gulp.src('./dist/umd/daVinci.js')
             .pipe(replace(/\|GitVersionNumber\|/, res))
-            .pipe(gulp.dest('./dist'));
+            .pipe(gulp.dest('./dist/umd'));
+    })
+    .catch((error) => {
+        console.log("Error in addVersionNumber gulp task", error);
+    });
+});
+
+gulp.task("addVersionNumber-amd", () => {
+    gitVersion.getFullVersionString()
+    .then((res) => {
+            return gulp.src('./dist/amd/daVinci.js')
+            .pipe(replace(/\|GitVersionNumber\|/, res))
+            .pipe(gulp.dest('./dist/amd'));
     })
     .catch((error) => {
         console.log("Error in addVersionNumber gulp task", error);
