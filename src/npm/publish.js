03e9dd2af0bb36fa93434e6c227a0b9cfa0fffe9
Ralf Hannuschka <ralf.hannuschka@akquinet.de>
2018-08-13 15:21:50 +0200

    fix codacy error

--- src/npm/publish.js
+++ src/npm/publish.js
@@ -60,9 +60,9 @@ npmVersionProcess.on("exit", async (exitCode) => {
     try {
         await spawnProcess("git", "add", "--all");
         await spawnProcess("git", "commit", "-m", `set new version to: ${packageVersionNumber}`);
-        await spawnProcess("git", 'tag', packageVersionNumber);
-        await spawnProcess("git", 'push');
-        await spawnProcess("git", 'push', 'origin', packageVersionNumber);
+        await spawnProcess("git", "tag", packageVersionNumber);
+        await spawnProcess("git", "push");
+        await spawnProcess("git", "push", "origin", packageVersionNumber);
         await spawnProcess(npmCommand, `publish`);
     } catch ( error ) {
         process.exit(1);
