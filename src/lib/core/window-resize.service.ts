c44a6b45c2a269e097f7ce543ab5c59c0a637863
Ralf Hannuschka <ralf.hannuschka@akquinet.de>
2019-04-18 16:30:20 +0200

    [FR] ListView #138 - add window resize service, resize cdk / scrollbars on window resize event

---
+++ src/lib/core/window-resize.service.ts
@@ -0,0 +1,108 @@
+
+import { Injectable, NgZone } from "@angular/core";
+import { Observable, Subject, fromEvent, Subscription, OperatorFunction } from "rxjs";
+
+/**
+ *
+ */
+@Injectable( { providedIn: "root" } )
+export class WindowResize {
+
+    /**
+     * window resize event stream
+     */
+    private windowResize$: Observable<Event>;
+
+    /**
+     * shared stream which one is registered on windowResize Stream
+     * all others will subscribe to shared stream so we can ensure
+     * only one window resize event exists
+     */
+    private shared$: Subject<any>;
+
+    /**
+     * current subscriber count on shared stream, the first subscriber
+     * will trigger shared stream registration on window resize stream
+     * if all subscribers are removed we automatically remove from
+     */
+    private subscriberCount = 0;
+
+    /**
+     * the subscription to window resize stream
+     * will be canceled if no one gets interrested anymore
+     */
+    private resizeSubscription: Subscription;
+
+    constructor(private zone: NgZone) {
+        this.windowResize$ = fromEvent( window, "resize" );
+        this.shared$ = new Subject();
+    }
+
+    /**
+     * create shared event stream and register to this
+     */
+    public onChange(): Observable<void> {
+        // subscribe
+        return Observable.create( observer => {
+            this.subscriberCount++;
+            const event$ = this.shared$.subscribe( observer );
+
+            if ( this.subscriberCount === 1 ) {
+                this.subscribeToWindowResizeEvent();
+            }
+
+            // unsubscribe
+            return () => {
+                event$.unsubscribe();
+                this.subscriberCount--;
+                if ( this.subscriberCount <= 0 ) {
+                    this.resizeSubscription.unsubscribe();
+                }
+            };
+        } );
+    }
+
+    /**
+     * register to window resize events
+     */
+    private subscribeToWindowResizeEvent(): void {
+        this.zone.runOutsideAngular( () => {
+            this.resizeSubscription = this.windowResize$
+                .pipe( debounceAnimationFrame() )
+                .subscribe( () => this.shared$.next() );
+        } );
+    }
+}
+
+function debounceAnimationFrame<T>(): OperatorFunction<T, T> {
+
+    return ( source$: Observable<T> ): Observable<T> => {
+
+        let resizeFired = false;
+        let drawing = false;
+
+        const requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
+
+        const obs = new Observable<T>( ( observer ) => {
+            function drawResize(): void {
+                if ( resizeFired === true ) {
+                    resizeFired = false;
+                    requestAnimationFrame( () => {
+                        observer.next();
+                        drawResize();
+                    } );
+                } else {
+                    drawing = false;
+                }
+            }
+
+            source$.subscribe( () => {
+                if ( drawing === false ) {
+                    resizeFired = true;
+                    drawResize();
+                }
+            } );
+        } );
+        return obs;
+    };
+}
--- src/lib/listview/components/listview.component.ts
+++ src/lib/listview/components/listview.component.ts
@@ -1,84 +1,97 @@
-import { Component, OnDestroy, Input, Output, EventEmitter, HostBinding, ViewChild } from "@angular/core";
-import { Subject } from "rxjs";
-import { ISelection } from "../api/selection.interface";
+import { Component, OnDestroy, Input, Output, EventEmitter, HostBinding, ViewChild, OnInit } from "@angular/core";
 import { SelectionModel } from "@angular/cdk/collections";
-import { IListItem } from "../api/list-item.interface";
-import { ListSource } from "../model/list-source";
-import { ViewportControl, Viewport } from "ngx-customscrollbar";
-import { ListOrientation } from "../api/list-config.interface";
 import { CdkVirtualScrollViewport } from "@angular/cdk/scrolling";
+import { ViewportControl } from "ngx-customscrollbar";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { WindowResize } from "../../core/window-resize.service";
+import { ISelection } from "../api/selection.interface";
+import { IListItem } from "../api/list-item.interface";
+import { ListOrientation } from "../api/list-config.interface";
+import { ListSource } from "../model/list-source";
 
-@Component({
+@Component( {
     selector: "davinci-listview",
     templateUrl: "listview.component.html",
     styleUrls: ["listview.component.scss"],
     viewProviders: [ViewportControl]
-})
-export class ListViewComponent<T> implements OnDestroy {
+} )
+export class ListViewComponent<T> implements OnDestroy, OnInit {
 
     @Output()
     public select: EventEmitter<ISelection>;
 
     @Input()
-    @HostBinding("class")
-    @HostBinding("class.davinci-listview")
+    @HostBinding( "class" )
+    @HostBinding( "class.davinci-listview" )
     public orientation: ListOrientation = "vertical";
 
-    @ViewChild(CdkVirtualScrollViewport)
+    @ViewChild( CdkVirtualScrollViewport )
     private cdkScrollViewport: CdkVirtualScrollViewport;
 
     public source: ListSource<T>;
 
     public selections: SelectionModel<IListItem<T>>;
 
+    /** subject which emits true if component gets destroyed */
     private destroy$: Subject<boolean> = new Subject();
 
     constructor(
         private viewportCtrl: ViewportControl,
+        private windowResize: WindowResize
     ) {
-        this.select     = new EventEmitter();
-        this.selections = new SelectionModel<IListItem<T>>(true);
+        this.select = new EventEmitter();
+        this.selections = new SelectionModel<IListItem<T>>( true );
     }
 
     @Input()
-    public set dataSource(source: ListSource<T>) {
-        if (source) {
-            this.addDataSource(source);
+    public set dataSource( source: ListSource<T> ) {
+        if ( source ) {
+            this.addDataSource( source );
         }
     }
 
-    public resize() {
+    public ngOnInit() {
+        this.windowResize.onChange()
+            .pipe( takeUntil( this.destroy$ ) )
+            .subscribe(
+                () => this.updateSize()
+            );
+    }
+
+    /** update size of listview */
+    public updateSize() {
         this.viewportCtrl.update();
         this.cdkScrollViewport.checkViewportSize();
     }
 
     public ngOnDestroy() {
-        this.destroy$.next(true);
+        this.destroy$.next( true );
         this.destroy$.complete();
     }
 
     /** if we click on an item select / deselect */
-    public itemClick(item: IListItem<T>) {
-        this.selections.isSelected(item)
-            ? this.deselectItem(item)
-            : this.selectItem(item);
+    public itemClick( item: IListItem<T> ) {
+        this.selections.isSelected( item )
+            ? this.deselectItem( item )
+            : this.selectItem( item );
     }
 
     /** select an item */
-    private selectItem(item: IListItem<T>) {
-        this.selections.select(item);
-        this.source.select(item);
+    private selectItem( item: IListItem<T> ) {
+        this.selections.select( item );
+        this.source.select( item );
     }
 
     /** deselect an item */
-    private deselectItem(item: IListItem<T>) {
-        this.selections.deselect(item);
-        this.source.deselect(item);
+    private deselectItem( item: IListItem<T> ) {
+        this.selections.deselect( item );
+        this.source.deselect( item );
     }
 
     /** add new datasource to our view */
-    private async addDataSource(source: ListSource<T>) {
-        if (this.source) {
+    private async addDataSource( source: ListSource<T> ) {
+        if ( this.source ) {
             await this.source.destroy();
         }
         this.source = await source.prepare();
